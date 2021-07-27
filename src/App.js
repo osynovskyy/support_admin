708bef769aadba09e28604000803d8d0fdd6b2b7
osynovskyy <panOsynovskyy@gmail.com>
2021-07-27 17:37:04 +0300

    wip

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Admin, EditGuesser, ListGuesser, Resource, ShowView } from 'react-admin';
 
 import jsonServerProvider from 'ra-data-json-server';
-import { UserList } from "./Users";
+import { UserList, UserShow } from "./Users";
 
 // const settings = {
 //   headers: {
@@ -15,7 +15,7 @@ const dataProvider = jsonServerProvider("https://my-json-server.typicode.com/osy
 
 const App = () => (
   <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={UserList} show={ShowView} />
+    <Resource name="users" list={UserList} show={UserShow} />
   </Admin>
 );
 
--- src/users.js
+++ src/users.js
@@ -1,19 +1,45 @@
 import * as React from "react";
-import { List, Datagrid, TextField, EmailField, BooleanField} from 'react-admin';
+import { List, Datagrid, Show, TextField, EmailField, BooleanField, DateField, SimpleShowLayout, Title} from 'react-admin';
 
 export const UserList = props => (
     <List {...props}>
-        <Datagrid rowClick="edit">
-            <TextField source="id" title="USER ID"/>
-            <EmailField source="email" title="EMAIL"/>
-            <TextField source="platform" title="PLATFORM"/>
-            <TextField source="client_version" title="CLIENT VERSION"/>
-            <TextField source="gender" title="GENDER"/>
-            <TextField source="fitness_level" title="FITNESS LEVEL"/>
-            <TextField source="language" title="LANGUAGE"/>
-            <BooleanField source="is_paid" title="PAID"/>
-            <BooleanField source="has_extra_purchases" title="EXTRA PURCHASES"/>
-            <TextField source="payment_info.product" title="PRODUCT"/>
+        <Datagrid rowClick="show">
+            <TextField source="id" label="USER ID"/>
+            <EmailField source="email" label="EMAIL"/>
+            <TextField source="platform" label="PLATFORM"/>
+            <TextField source="client_version" label="CLIENT VERSION"/>
+            <TextField source="gender" label="GENDER"/>
+            <TextField source="fitness_level" label="FITNESS LEVEL"/>
+            <TextField source="language" label="LANGUAGE"/>
+            <BooleanField source="is_paid" label="PAID"/>
+            <BooleanField source="has_extra_purchases" label="EXTRA PURCHASES"/>
+            <TextField source="payment_info.product" label="PRODUCT"/>
         </Datagrid>
     </List>
+);
+
+const PostTitle = ({ record }) => {
+    return <span>{record.id}</span>;
+};
+
+export const UserShow = (props) => (
+    <Show title={<PostTitle/>} {...props} >
+        <SimpleShowLayout>
+            <TextField source="id" label="USER ID"/>
+            <EmailField source="email" label="EMAIL"/>
+            <TextField source="platform" label="PLATFORM"/>
+            <TextField source="client_version" label="CLIENT VERSION"/>
+            <TextField source="gender" label="GENDER"/>
+            <TextField source="fitness_level" label="FITNESS LEVEL"/>
+            <TextField source="language" label="LANGUAGE"/>
+            <BooleanField source="is_paid" label="PAID"/>
+            <BooleanField source="has_extra_purchases" label="EXTRA PURCHASES"/>
+            <Title />
+            <TextField source="payment_info.product" label="PRODUCT"/>
+            <DateField source="payment_info.original_purchase_date" label="ORIGINAL PURCHASE DATE"/>
+            <DateField source="payment_info.purchase_date" label="PURCHASE DATE"/>
+            <DateField source="payment_info.expires_date" label="EXPIRES DATE"/>
+            <BooleanField source="payment_info.is_trial_period" label="TRIAL PERIOD"/>
+        </SimpleShowLayout>
+    </Show>
 );
